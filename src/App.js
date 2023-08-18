744e17a23a90e27437585fe9fe6ad8cda1c52912
Batuhan Ozturk <fbatuhanr@gmail.com>
2023-08-18 11:28:34 +0300

    app js edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>
+          Example heading
+          <Badge bg="secondary" as={Button}>
+            New
+          </Badge>
+        </h1>
+      </div>
   );
 }
 
