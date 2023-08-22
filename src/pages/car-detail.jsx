3545913c34ef6025dea12cb2804a87e8841c7722
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-22 17:48:54 +0300

    320d

--- src/pages/car-detail.jsx
+++ src/pages/car-detail.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
-
+import ListGroup from 'react-bootstrap/ListGroup';
 import {useParams} from "react-router-dom";
 
 import data from '../DATA/data.json';
 import {getNextKeyDef} from "@testing-library/user-event/dist/keyboard/getNextKeyDef";
+import { Col, Container, Row } from 'react-bootstrap';
 
 const CarDetail = () => {
 
@@ -18,20 +19,29 @@ const CarDetail = () => {
     },[]);
 
     return (
-        <div className='main'>,
+        <Container>,
             {
                 modelProperties ?
-                    <div>
-                        <p>{carBrand} - {carModel}</p>
-                        <p>{modelProperties.power}</p>
-                        <p>{modelProperties.engineSize}</p>
-                        <p>{modelProperties.gearbox}</p>
-                        <p>{modelProperties.bodyType}</p>
-                        <p>{modelProperties.fuelType}</p>
-                    </div>
+                    <Row>
+                        <Col><img src={modelProperties.imageUrl}/></Col>
+                        <Col>
+                        <ListGroup>
+                            <ListGroup.Item>Brand: - Model:  <b>{carBrand} - {carModel}</b></ListGroup.Item>
+                            <ListGroup.Item>HP: {modelProperties.power}</ListGroup.Item>
+                            <ListGroup.Item>Engine Size: {modelProperties.engineSize}</ListGroup.Item>
+                            <ListGroup.Item>Gear Box: {modelProperties.gearbox}</ListGroup.Item>
+                            <ListGroup.Item>Body Type: {modelProperties.bodyType}</ListGroup.Item>
+                            <ListGroup.Item> Fuel Type: {modelProperties.fuelType}</ListGroup.Item>
+                        </ListGroup>
+                        <p></p>
+                        
+                        
+                        </Col>
+                        
+                    </Row>
                     : null
             }
-        </div>
+        </Container>
     );
 };
 
